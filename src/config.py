f1e80945ff8d6133ecab98dffb2a84c8f63a4c54
AleKolar <157892507+AleKolar@users.noreply.github.com>
2025-02-07 20:41:24 +0300

    Celery mailing added_1

--- src/config.py
+++ src/config.py
@@ -12,21 +12,18 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    SECRET_KEY: str = os.environ.get("SECRET_KEY", default="SECRET_KEY")
+    ALGORITHM: str = os.environ.get("ALGORITHM", default="ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", default=180))
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
     )
 
     def get_db_url(self):
-        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"  
+        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
                 f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
 
-    SECRET_KEY = os.environ.get("SECRET_KEY", default="SECRET_KEY")
-    ALGORITHM = os.environ.get("ALGORITHM", default="ALGORITHM")
-    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", default=180))
 
 settings = Settings()
 
