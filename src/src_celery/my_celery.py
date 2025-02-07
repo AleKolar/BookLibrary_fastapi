1c62571a3ff0d9ba162b71df81334f23004683ef
AleKolar <157892507+AleKolar@users.noreply.github.com>
2025-02-07 09:40:24 +0300

    Celery mailing added

--- src/src_celery/my_celery.py
+++ src/src_celery/my_celery.py
@@ -1,14 +1,19 @@
 from celery import Celery
 
 
-celery_app = Celery(
-    'src',
-    broker='amqp://guest:guest@localhost:5672',
-    backend='rpc://',
-)
+def make_celery():
+    app = Celery(
+        'src',
+        broker='amqp://guest:guest@localhost:5672',  # URL сообщения брокера
+        backend='rpc://'  # Бэкенд для подсчета результатов
+    )
 
-celery_app.conf.update(
-    task_routes={
-        'app.tasks.send_email': 'default',
-    }
-)
+    app.conf.update(
+        task_routes={
+            'app.tasks.send_email': 'default',
+        },
+    )
+
+    return app
+
+celery_app = make_celery()
